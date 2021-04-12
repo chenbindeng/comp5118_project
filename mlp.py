4715e7bf8dace5b46f75b444c87cc7d2b8e40bd9
Ben <75921105+chenbindeng@users.noreply.github.com>
2021-04-11 20:12:13 -0400

    Add files via upload

--- mlp.py
+++ mlp.py
@@ -1,6 +1,7 @@
 from tensorflow.python.keras import *
 from tensorflow.python.keras.layers import *
 from tensorflow.python.keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
 from generations import *
 import tensorflow as tf
 from utility import *
@@ -38,47 +39,98 @@ def mlp(input):
     temp = BatchNormalization()(temp)
     temp = Dense(n_hidden_3, activation='relu')(temp)
     temp = BatchNormalization()(temp)
+    temp = Dense(n_output, activation='relu')(temp)
+    temp = BatchNormalization()(temp)
     out_put = Dense(n_output, activation='sigmoid')(temp)
     model = Model(input_bits, out_put)
-    model.compile(optimizer='adam', loss='mse', metrics=[bit_err])
-    model.summary()
     return model
 
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
-##lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, verbose=0, patience=5, min_lr=0.5e-6)
-lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=1, cooldown=0, verbose=0, patience=5, min_lr=0.5e-6)
-checkpoint = callbacks.ModelCheckpoint('./result/mlp_temp_trained_25_8_pilot.h5', monitor='val_bit_err',
-                                       verbose=0, save_best_only=True, mode='min', save_weights_only=True)
+base_model_es = EarlyStopping(monitor='val_bit_err', mode='min', verbose=1, patience=10)
+base_model_lr_reducer = ReduceLROnPlateau(monitor='val_bit_err', factor=np.sqrt(0.1), cooldown=0, 
+                                          verbose=1, patience=10, min_lr=0.5e-6)
 
-model = mlp(input_bits)
+base_model_checkpoint = callbacks.ModelCheckpoint('./result/mlp_temp_trained_25_8_pilot_lr_no_es.h5', monitor='val_bit_err',
+                                                      verbose=0, save_best_only=True, mode='min', save_weights_only=False)
+checkPointList = [base_model_checkpoint]                                                  
+checkPointList += [base_model_es]
+checkPointList += [base_model_lr_reducer]
+    
+base_model = mlp(input_bits)
+base_model.compile(optimizer='adam', loss='mse', metrics=[bit_err])
+print("Number of layers in the base model: ",len(base_model.layers))
 training_time_total = 0
 training_time_start = time.time()
-epochs = 1000     
-model_info = model.fit_generator(
-    training_gen(1000, False, 25),
+initial_epochs = 10
+epochs = 500
+base_model_info = base_model.fit(
+    training_gen(1000, False, 10),
     steps_per_epoch=50,
     epochs=epochs,
-    validation_data=validation_gen(1000, False, 25),
+    validation_data=validation_gen(1000, False, 10),
     validation_steps=1,
-    callbacks=[lr_reducer, es, checkpoint],
+    callbacks = checkPointList, 
     verbose=2)
-print(model_info.history.keys())
+print(base_model_info.history.keys())
+
 duration = time.time() - training_time_start
 training_time_total += duration
 
 training_average_time = training_time_total / epochs
 print("average_time: ", training_average_time)
+sio.savemat('./result/base_model_training_history.mat', base_model_info.history)
+plot_model_history(base_model_info)
 
-sio.savemat('./result/mlp_model_training_history.mat', model_info.history)
-plot_model_history(model_info)
-model.load_weights('./result/mlp_temp_trained_25_8_pilot.h5')
+# fine tunning
+model = mlp(input_bits)
+model.load_weights('./result/mlp_temp_trained_25_8_pilot_lr_no_es.h5')
+
+base_learn_rate =  base_model_info.history['lr']
+
+adam = Adam(lr = np.array(base_learn_rate)[-1]/2)
+
+model.trainable = True
+fine_tune_at = 7
+for layer in model.layers[:fine_tune_at]:
+    layer.trainable =  False
+
+model.compile(optimizer=adam, loss='mse', metrics=[bit_err])
+fine_tuning_es = EarlyStopping(monitor='val_bit_err', mode='min', verbose=1, patience=20)
+fine_tuning_lr_reducer = ReduceLROnPlateau(monitor='val_bit_err', factor=np.sqrt(0.1), cooldown=0, verbose=1, patience=5, min_lr=0.1e-6)
+fine_tuning_checkpoint = callbacks.ModelCheckpoint('./result/fine_tuning_trained.h5', monitor='val_bit_err',
+                                                    verbose=0, save_best_only=True, mode='min', save_weights_only=False)
+model.summary()
+print("mode trainable parameters: ", len(model.trainable_variables))
+
+fine_tune_epochs = 100
+base_epoch = base_model_info.epoch[-1]
+print (base_epoch)
+total_epochs =  base_epoch + fine_tune_epochs
+print(total_epochs)
+
+checkList = [fine_tuning_checkpoint]
+checkList += [fine_tuning_es]
+checkList += [fine_tuning_lr_reducer]
+
+history_fine = model.fit(training_gen(1000, False, 25),
+                        steps_per_epoch=50,
+                        epochs=total_epochs,
+                        initial_epoch = base_epoch,
+                        validation_data=validation_gen(1000, False, 25),
+                        validation_steps=1,
+                        callbacks=checkList,
+                        verbose=2)
+sio.savemat('./result/fine_tuning_history.mat', history_fine.history)
+plot_fine_tuning_model_history(base_model_info, history_fine)
+
+model = mlp(input_bits)
+model.load_weights('./result/fine_tuning_trained.h5')
 test_time_total = 0
 test_time_start = time.time()
 BER = []
 snr_in_db = np.arange(5, 30, 5)
-for SNR in snr_in_db:
+for snr in snr_in_db:
     y = model.evaluate(
-        validation_gen(10000, False, SNR),
+        validation_gen(10000, False, snr),
         steps=1
     )
     BER.append(y[1])
@@ -89,11 +141,15 @@ test_time_total += duration
 
 test_average_time = test_time_total / len(snr_in_db)
 print("average_time: ", test_average_time)
-plt.semilogy(snr_in_db, BER)
+plt.semilogy(BER, "-*")
 plt.grid(True)
-plt.ylim(0.0001, 1)
+plt.ylim(0.001, 0.2)
 plt.xlabel('SNR (dB)')
 plt.ylabel('BER')
 plt.show()
 BER_matlab = np.array(BER)
-sio.savemat('./result/mlp_result_8_pilot.mat', {'BER':BER_matlab, 'training_average_time':training_average_time, 'test_average_time':test_average_time})
+sio.savemat('./result/mlp_result_8_pilot_lr_no_es.mat', {'BER':BER_matlab, 'training_average_time':training_average_time, 'test_average_time':test_average_time})
+
+#base_model_train(10, True, True)
+#fine_tune_model(15,True, True)
+
--- utility.py
+++ utility.py
@@ -8,21 +8,111 @@ import time
 import os
 
 def plot_model_history(model_history):
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    fig, axs = plt.subplots(1,3,figsize=(15,5))
     # summarize history for accuracy
-    axs[0].plot(range(1,len(model_history.history['bit_err'])+1),model_history.history['bit_err'])
-    axs[0].plot(range(1,len(model_history.history['val_bit_err'])+1),model_history.history['val_bit_err'])
-    axs[0].set_title('Model Accuracy')
-    axs[0].set_ylabel('Accuracy')
-    axs[0].set_xlabel('Epoch')
+    axs[0].semilogy(range(1,len(model_history.history['bit_err'])+1),model_history.history['bit_err'])
+    axs[0].semilogy(range(1,len(model_history.history['val_bit_err'])+1),model_history.history['val_bit_err'])
+    axs[0].set_title('Model Bit Error')
+    axs[0].set_ylabel('Bit Error')
+    axs[0].set_xlabel('Epochs')
     axs[0].set_xticks(np.arange(1,len(model_history.history['bit_err'])+1),len(model_history.history['bit_err'])/10)
-    axs[0].legend(['train', 'val'], loc='best')
+    axs[0].legend(['training', 'validation'], loc='best')
+    axs[0].grid(True, which="both")
     # summarize history for loss
-    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].semilogy(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].semilogy(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
     axs[1].set_title('Model Loss')
     axs[1].set_ylabel('Loss')
     axs[1].set_xlabel('Epochs')
     axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
-    axs[1].legend(['train', 'val'], loc='best')
+    axs[1].legend(['training', 'validation'], loc='best')
+    axs[1].grid(True, which="both")
+    axs[2].set_title('Learning')
+    axs[2].semilogy(range(1,len(model_history.history['lr'])+1),model_history.history['lr'])
+    print("bit_err min: ", np.min(model_history.history['bit_err']))
+    print("val_bit_err: ", np.min(model_history.history['val_bit_err']))
+    print("loss: ", np.min(model_history.history['loss']))
+    print("val_loss: ", np.min(model_history.history['val_loss']))
+    print("lr min: ", np.min(model_history.history['lr']))
+    axs[2].set_ylabel('Learning Rate')
+    axs[2].set_xlabel('Epochs')
+    axs[2].set_xticks(np.arange(1,len(model_history.history['lr'])+1),len(model_history.history['lr'])/10)
+    axs[2].legend(['Learning Rate'], loc='best')
+    axs[2].grid(True, which="both")
+
+    plt.show()
+
+def plot_fine_tuning_model_history(base_model_info, history_fine):
+    bit_err = base_model_info.history['bit_err']
+    val_bit_err = base_model_info.history['val_bit_err']
+
+    loss = base_model_info.history['loss']
+    val_loss = base_model_info.history['val_loss']
+
+    lr = base_model_info.history['lr']
+
+    base_model_epoch = len(lr)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(3, 1, 1)
+    plt.semilogy(bit_err, label='Training bit error')
+    plt.semilogy(val_bit_err, label='Validation bit error')
+    plt.legend(loc='upper right')
+    plt.ylabel('bit error')
+    plt.ylim([min(plt.ylim()),0.2])
+    plt.title('Training and Validation bit error')
+
+    plt.subplot(3, 1, 2)
+    plt.semilogy(loss, label='Training Loss')
+    plt.semilogy(val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.ylabel('Cross Entropy')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
+    
+    bit_err += history_fine.history['bit_err']
+
+    val_bit_err += history_fine.history['val_bit_err']
+
+    loss += history_fine.history['loss']
+    val_loss += history_fine.history['val_loss']
+
+    lr +=  history_fine.history['lr']
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(3, 1, 1)
+    plt.semilogy(bit_err, label='Training bit error')
+    plt.semilogy(val_bit_err, label='Validation bit error')
+    #plt.ylim([0.8, 1])
+    plt.plot([base_model_epoch-1, base_model_epoch-1],
+          plt.ylim(), label='Start Fine Tuning')
+    plt.legend(loc='upper right')
+    plt.grid(True, which='major')
+    plt.ylabel('Bit Error')
+    #plt.xlabel('epoch')
+    plt.title('Training and bit error')
+
+    plt.subplot(3, 1, 2)
+    plt.semilogy(loss, label='Training Loss')
+    plt.semilogy(val_loss, label='Validation Loss')
+    #plt.ylim([0, 1.0])
+    plt.plot([base_model_epoch-1, base_model_epoch-1],
+             plt.ylim(),label='Start Fine Tuning')
+    plt.legend(loc='upper right')
+    #plt.title('Training and Validation Loss')
+    #plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.grid(True, which='major')
+
+    plt.subplot(3, 1, 3)
+    plt.semilogy(lr, label='Adaptive Learning Rate')
+    #plt.ylim([0, 1.0])
+    plt.plot([base_model_epoch-1, base_model_epoch-1],
+             plt.ylim(),label='Start Fine Tuning')
+    plt.legend(loc='upper right')
+    #plt.title('ALR')
+    plt.xlabel('epoch')
+    plt.ylabel('Learning Rate')
+    plt.grid(True, which='major')
     plt.show()
